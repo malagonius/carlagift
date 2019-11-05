dfd002289c53c708c983d3cc5b481debaa8a8a7e
malagonius <47065208+malagonius@users.noreply.github.com>
2019-11-05 14:47:31 +0100

    Add files via upload

---
+++ carla/index.php
@@ -0,0 +1,155 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="btd.css">
+	<title>Tanti Auguri</title>
+</head>
+<body>
+	<h1>Auguri Carla</h1>
+	<div id="cover"></div>
+	<div class="title_container">
+		<div class="title_image"></div>
+	</div>
+	<div class="content_container">
+		<div id="navigation" class="navigation">
+			<div class="navigation_container">
+				<a class="active" id ="1" href="#"><span>Rules</span></a>
+				<a id ="2" href="#"><span>Content</span></a>
+				<a id ="3" href="#"><span>Riconoscimenti</span></a>		
+			</div>
+		</div>
+			<div id="content1" class="content active">
+				<div>
+					<span>
+						<ul><h3>Tantissimi auguri</h3>
+								<ul>Complimenti per aver decifrato il codice e per aver sbloccato il sito con successo.
+								<br>
+									Ora il prossimo passo per recalmare il tuo regalo è semplice, basta premere sul tasto reclama
+								<br>Magari è veramente cosi semplice, o forse è solo una trappola</li>
+								<br>Per scoprirlo ti tocca provare, ma se fossi in te non lo farei
+
+								<p>Ad ogni modo, che finisca bene o finisca male, ci tenevamo ad augurarti tanti auguri e tanta felicità a te e ai tuoi cari</p>
+						</ul>
+								</li>
+							</ul>
+						</ul>
+					</span>
+				</div>		
+			</div>
+			<div id="content2" class="content">	
+				<div>
+					<span>
+						<ul>
+							<li>4 Type of Monkeys</li>
+							<ul>
+								<li>
+									Standard --> Very High Quality/Price value Strong most cases, low efficency, can't pop lead
+									<ul>
+										<li>Dart<ul>
+											<li><a id="dart1">Sharp Shooting > Spiked Balls > Ultra Juggernaut</a></li>
+											<li><a id="dart2"></a>Incrase Range > Extra firing > Crossbow Master</li>
+										</ul></li>
+										<li>Boomerang<ul>
+											<li><a id="rang1"></a>SharpRang > FlameRang > LaserRang</li>
+											<li><a id="rang2"></a> ExtraRange > Epic Range > Permacharge</li>
+										</ul></li>
+										<li>TackShooter<ul>
+											<li><a id="tack1"></a>More Tacks > Even More Tacks > Tack-nator</li>
+											<li><a id="tack2"></a>Hard tacks > Rusty Tacks > Huge Tacks</li>
+										</ul></li>
+									</ul>
+								<li>Military --> Can pop Lead, Good for Crowd Controll, can be placed almost everywhere
+									<ul>
+										<li>Sniper<ul>
+											<li><a id="snip1"></a>Increased Accuracy > Deadly Precision > Full Metal Jacket</li>
+											<li><a id="snip2"></a>Faster Firing  > Semi Automatic Rifle > Elite Defender</li>
+										</ul></li>
+										<li>Submarine<ul>
+											<li><a id="subb1"></a>Rocket Launcher > First Strike > Nuclear Missle</li>
+											<li><a id="subb2"></a> Submerge > Radioactive Submarine > Atomic radiation</li>
+										</ul></li>
+										<li>HeilPilot<ul>
+											<li><a id="pilo1"></a>Faster firing > Razor Rotors > Apache Prime</li>
+											<li><a id="pilo2"></a>Ultra Jet > Chase > Comache Defence</li>
+										</ul></li>
+									</ul></li>
+								<li>Magical --> Very expensive, High efficency,Can't pop certain bloons
+									<ul>
+										<li>Wizzard<ul>
+											<li><a id="wizz1"></a>Intese Magic > Arcane Spikes > Archmage</li>
+											<li><a id="wizz2"></a>Fire Ball > Wall Of Fire > PoenixLord</li>
+										</ul></li>
+										<li>Druid<ul>
+											<li><a id="drud1"></a>Chain Lighting > Lighting Ball > StormLord</li>
+											<li><a id="drud2"></a>Hard Throns > Grasping Thorns > Spirit Of The Forest</li>
+										</ul></li>
+										<li>SuperMonkey<ul>
+											<li><a id="supm1"></a>Laser Vision > Plasma Ray > Sun God</li>
+											<li><a id="supm2"></a>Epic Range > Robo Monkey > BloonsDay</li>
+										</ul></li>
+									</ul></li>
+								<li>Support --> can't pop bloons, give cool and unique effects
+									<ul>
+										<li>Banana Farm<ul>
+											<li><a id="farm1"></a>Increase Production > Banana Republic > Bananopoli</li>
+											<li><a id="farm2"></a>Valuable Banana > Banananomics > BnanaBank</li>
+										</ul></li>
+										<li>Monkey Village<ul>
+											<li><a id="vill1"></a>Jungle Drums > Camo Detections > Inspiring Citadel</li>
+											<li><a id="vill2"></a>Banana Accolyte > Banana Temple > Golden Banana</li>
+										</ul></li>
+									</ul></li>
+							</ul>
+						</ul>
+					</span>
+				</div>			
+			</div>	
+			<div id="content3" class="content star-wars">	
+				<div>
+					<span>
+						<div class="fade"></div>
+						<section class="star-wars">
+						  <div class="crawl">
+						    <div class="title">
+						      <p>Episode 38</p>
+						    </div>
+						    
+						    <p>Una stella è nata nell'universo</p>
+
+					    	<p>Più di 38 anni fa, una bambina, una speranza è venuta qui nel pianeta chiamato terra</p>
+						      
+						     <p>Tra le grandi battaglie, ed eventuali tumulti l'umanità ha bisogno di una luce su cui puntare</p>
+
+						    <p>Grazie Carla e tanti auguri di nuovo</p>
+
+						    <p>Amministratore Delegato : Antonina Lo Piparo</p>
+						    <p>Principale Finanziatore : Renato Nicotra</p>
+						    <p>Regia : Renato Nicotra</p>
+						    <p>Ideatore e sceneggaiture : Renato Nicotra</p>
+						    <p>Lead Design : Mattia Nicotra</p>
+						    <p>Lead Developer : Mattia Nicotra</p>
+						    <p>Supporto Morale : Antonia Lo Piparo</p>	 
+						</section>
+					</span>
+				</div>
+			</div>	
+			<div id="content4" class="content">
+				<div>
+					<span>
+						Telephone : 1234567890<br>
+						Email : t.o@fregato.ora<br>
+						Fax : lol<br>
+						Address : Viale Manidal, Naso 1337
+					</span>
+				</div>		
+			</div>		
+		</div>
+	</div>
+	<div class="modal">
+		<div class="modal_container">
+			<div class="modal_title"></div>
+			<div class="modal_content"></div>
+		</div>
+	</div>
+</body>
+<script type="text/javascript" src="btd.js"></script>
+</html>
